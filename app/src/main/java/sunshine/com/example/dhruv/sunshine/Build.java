6daa9357cc4a1cb2b28fb610685e846b7051ac1c
dhruv <dhruv.learner@gmail.com>
2016-05-29 12:46:06 +0530

    Finished with content providers

---
+++ app/src/main/java/sunshine/com/example/dhruv/sunshine/Build.java
@@ -0,0 +1,10 @@
+package sunshine.com.example.dhruv.sunshine;
+
+/**
+ * Created by Dhruv on 5/27/2016.
+ */
+public class Build {
+
+    public static final String OPEN_WEATHER_MAP_API_KEY = "2f8bf9ecd4fc406d1e0e8632c2e6d9ff";
+
+}
